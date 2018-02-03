eaee38a7bae202585cb1a42768395f8e2b38c4d4
vanthuc96 <32368671+vanthuc96@users.noreply.github.com>
2018-02-03 18:30:19 +0700

    Add files via upload

---
+++ src/main/java/com/example/admin/bai2/activity_1.java
@@ -0,0 +1,13 @@
+package com.example.admin.bai2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
